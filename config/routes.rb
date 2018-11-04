d078e469ef1cd5163c34e5dc7d5002d622153b5e
Artem Bebyk <artem.bebyk@gmail.com>
2018-11-04 20:26:41 +0000

    creat new template, action and controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   
   
-  
+  resources :articles
 
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
