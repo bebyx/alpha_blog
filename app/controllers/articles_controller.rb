33d28587f6ca8da00ed6651a40f80d1b322e8b28
Artem Bebyk <artem.bebyk@gmail.com>
2018-11-05 19:34:03 +0000

    added index, updated show, new, edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,47 @@
-class ArticlesController < ActionController::Base
+class ArticlesController < ApplicationController
+   
+   def index
+      @articles = Article.all
+   end
+   
    
    def new
        @article = Article.new
-       
+   end
+   
+   def edit
+      @article = Article.find(params[:id])
+   end
+   
+   def update
+      @article = Article.find(params[:id])
+      
+      if @article.update(article_params)
+       flash[:notice] = "The article was updated"
+       redirect_to article_path(@article)
+       else
+       render 'edit'
+       end
+      
    end
    
    def create
-       #render plain: params[:article].inspect 
        
        @article = Article.new(article_params)
-       @article.save
-       redirect_to articles_show(@article)
        
+       if @article.save
+       flash[:notice] = "A new article was created"
+       redirect_to article_path(@article)
+       else
+       render 'new'
+       end
+       
+   end
+   
+   def show
+   
+      @article = Article.find(params[:id])
+      
    end
    
    private
